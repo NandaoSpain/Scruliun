57309ed4f8165808d105c2b50031c43e39e05f9a
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2025-02-01 17:42:31 -0300

    sessions controller

---
+++ src/controllers/sessions-controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+import { AppError } from "@/utils/AppError";
+import { compare } from "bcrypt";
+import { authConfig } from "@/config/auth";
+import { sign } from "jsonwebtoken";
+
+class SessionsController {
+  async create(request: Request, response: Response): Promise<void> {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+    const { email, password } = bodySchema.parse(request.body);
+
+    const user = await prisma.users.findFirst({ where: { email } });
+
+    if (!user) {
+      throw new AppError("Invalid email or password", 401);
+    }
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("Invalid email or password", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({ role: user.role ?? "user" }, secret, {
+      subject: user.id,
+      expiresIn
+    });
+
+    const {password: hashedPassword, ...userWithoutPassword} = user
+
+    response.status(200).json({ token, user: userWithoutPassword });
+  }
+}
+
+export { SessionsController };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import { usersRoutes } from "./users-routes";
+import { sessionsRoutes } from "./sessions-controller";
 
 const routes = Router()
 
-routes.use("/users", () => {
-  throw new Error("test error: missing")
-}, usersRoutes)
+routes.use("/users", usersRoutes)
+routes.use("/sessions", sessionsRoutes)
+
 
 export { routes }
