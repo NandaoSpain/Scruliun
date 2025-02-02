09bb4c978946554025fc58dede3ae24f7f5002bd
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2025-02-02 17:14:27 -0300

    feat: ensure authenticated + verify user authorization role

--- src/routes/tasks-controller-routes.ts
+++ src/routes/tasks-controller-routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express"
 import { TasksController } from "@/controllers/tasks-controller"
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization"
 
 const tasksRoutes = Router()
 
 const tasksController = new TasksController()
 
+tasksRoutes.use(ensureAuthenticated)
 tasksRoutes.get("/", tasksController.index)
 tasksRoutes.post("/", tasksController.create)
 tasksRoutes.put("/:id", tasksController.show)
-tasksRoutes.patch("/:id", tasksController.update)
+tasksRoutes.patch("/:id",verifyUserAuthorization(["admin"]), tasksController.update)
 
 export { tasksRoutes }
