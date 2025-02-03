3b392514ff2c03a31f4dbe204eb233dc36af3cc0
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2025-02-03 17:15:09 -0300

    Update tasks-controller-routes.ts

--- src/routes/tasks-controller-routes.ts
+++ src/routes/tasks-controller-routes.ts
@@ -1,16 +1,20 @@
-import { Router } from "express"
-import { TasksController } from "@/controllers/tasks-controller"
-import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
-import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization"
+import { Router } from "express";
+import { TasksController } from "@/controllers/tasks-controller";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
 
-const tasksRoutes = Router()
+const tasksRoutes = Router();
 
-const tasksController = new TasksController()
+const tasksController = new TasksController();
 
-tasksRoutes.use(ensureAuthenticated)
-tasksRoutes.get("/", tasksController.index)
-tasksRoutes.post("/", tasksController.create)
-tasksRoutes.put("/:id", tasksController.show)
-tasksRoutes.patch("/:id",verifyUserAuthorization(["admin"]), tasksController.update)
+tasksRoutes.use(ensureAuthenticated);
+tasksRoutes.get("/", tasksController.index);
+tasksRoutes.post("/", tasksController.create);
+tasksRoutes.put("/:id", tasksController.show);
+tasksRoutes.patch(
+  "/:id",
+  verifyUserAuthorization(["admin"]),
+  tasksController.update
+);
 
-export { tasksRoutes }
+export { tasksRoutes };
