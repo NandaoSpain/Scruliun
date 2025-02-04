529705fb18f03edb7fe70b55421ed62662bfd875
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2025-02-04 15:37:47 -0300

    feat: team controller delete

--- src/controllers/teams-controller.ts
+++ src/controllers/teams-controller.ts
@@ -3,6 +3,16 @@ import { z } from "zod";
 import { prisma } from "@/database/prisma";
 
 class TeamsController {
+  async index(request: Request, response: Response): Promise<any> {
+    const teams = await prisma.teams.findMany();
+
+    if (!teams) {
+      return response.status(404).json({ message: "No teams found" });
+    }
+
+    response.status(200).json(teams);
+  }
+
   async create(request: Request, response: Response) {
     const bodySchema = z.object({
       name: z.string().trim(),
@@ -24,16 +34,36 @@ class TeamsController {
       description: z.string(),
     });
     const { name, description } = bodySchema.parse(request.body);
-    const team = await prisma.teams.update({
-      where: { id },
-      data: { name, description },
-    });
     const teamExists = await prisma.teams.findFirst({ where: { id } });
 
     if (!teamExists)
       return response.status(404).json({ message: "Team not found" });
 
+    const team = await prisma.teams.update({
+      where: { id },
+      data: { name, description },
+    });
+
     response.status(200).json(team);
+  }
+  async delete(request: Request, response: Response): Promise<any> {
+    const { id } = request.params;
+    const teamExists = await prisma.teams.findUnique({
+      where: { id },
+      include: { Tasks: true },
+    });
+    if (!teamExists) {
+      return response.status(404).json({ message: "Team not found" });
+    }
+
+    if (teamExists.Tasks.length > 0) {
+      return response
+        .status(400)
+        .json({ message: "Cannot delete team with associated tasks" });
+    }
+    await prisma.teams.delete({ where: { id } });
+
+    response.status(200).json({ message: "Team deleted successfully" });
   }
 }
 export { TeamsController };
--- src/routes/teams-controller.ts
+++ src/routes/teams-controller.ts
@@ -4,7 +4,9 @@ import { TeamsController } from "@/controllers/teams-controller";
 const teamsRoutes = Router()
 const teamsController = new TeamsController()
 
+teamsRoutes.get("/", teamsController.index)
 teamsRoutes.post("/", teamsController.create)
 teamsRoutes.patch("/:id", teamsController.update)
+teamsRoutes.delete("/:id", teamsController.delete)
 
 export { teamsRoutes }
